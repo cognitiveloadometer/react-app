bd5dc8a62edf4fe2d5c38c6b9c1267bf45ce82eb
Tales <jobbtales@gmail.com>
2022-09-16 12:59:07 -0300

    prod

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -8,6 +8,7 @@ import api from "../services/api"
 export const HomePage = ({setAuthenticated, setUserData}) => {
     const local_client_id = '875942057415-d1bvi4i8t8qkv2eb4murottvre0sen4j.apps.googleusercontent.com'
     const deploy_client_id = '510816376649-ts8v56n2rjvoglm6is11jlllufc2ujgc.apps.googleusercontent.com'
+    const vercel_teste = '954097058141-aigdukkmbe6j7didd0guipri2mqcc1mr.apps.googleusercontent.com'
 
     let responseName = ''
     let responseEmail = ''
@@ -19,13 +20,13 @@ export const HomePage = ({setAuthenticated, setUserData}) => {
         onSubmit()
     }
 
-    const onSubmit = async () => {
+    const onSubmit = () => {
       const data = {
         name: responseName,
         email: responseEmail
       }
 
-      await api.post(`/users/login`, data).then(response => setUserData(response.data)).catch(error => console.log(error.response.data))
+      api.post(`/users/login`, data).then(response => setUserData(response.data)).catch(error => console.log(error.data))
       return setAuthenticated(true)
     }
 
